1c1eb2a6642b854678b1f9193221ac474bcc5db6
Ozan1535 <ozanbilgic1535@gmail.com>
2025-08-09 11:39:23 +0300

    Change name

--- web-arazial/src/components/layout/Navbar.js
+++ web-arazial/src/components/layout/Navbar.js
@@ -647,7 +647,7 @@ const Navbar = () => {
                     d="M9 5H7a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2V7a2 2 0 00-2-2h-2M9 5a2 2 0 002 2h2a2 2 0 002-2M9 5a2 2 0 012-2h2a2 2 0 012 2"
                   />
                 </svg>
-                Tekliflerim
+                İşlemlerim
               </NavLink>
               {isAdmin && (
                 <NavLink to="/admin/dashboard">
@@ -867,7 +867,7 @@ const Navbar = () => {
                     d="M9 5H7a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2V7a2 2 0 00-2-2h-2M9 5a2 2 0 002 2h2a2 2 0 002-2M9 5a2 2 0 012-2h2a2 2 0 012 2"
                   />
                 </svg>
-                Tekliflerim
+                İşlemlerim
               </MobileNavLink>
               {isAdmin && (
                 <MobileNavLink
